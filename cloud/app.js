1452a86489631be6925c5bbabe58e7d8274ddc41
KIMBOX <kimbox8866@gmail.com>
2015-06-04 12:41:20 +0800

    1.0.1
    1. 增加routes路由文件等
    2. 增加bootstrap文件

--- cloud/app.js
+++ cloud/app.js
@@ -1,15 +1,20 @@
 // 在 Cloud code 里初始化 Express 框架
-var express = require('express');
+var express = require('express'),
+    routes = require('cloud/routes'),
+    path = require('path');
+
 var app = express();
 
 // App 全局配置
 app.set('views','cloud/views');   // 设置模板目录
 app.set('view engine', 'ejs');    // 设置 template 引擎
 app.use(express.bodyParser());    // 读取请求 body 的中间件
+// app.use(express.static(path.join(__dirname, 'public')));
 
-// 使用 Express 路由 API 服务 /hello 的 HTTP GET 请求
+// 设置路由
+app.get('/', routes.index);
 app.get('/hello', function(req, res) {
-  res.render('hello', { message: 'Congrats, you just set up your app!' });
+	res.render('hello', { message: 'Congrats, you just set up your app!' });
 });
 
 // 最后，必须有这行代码来使 express 响应 HTTP 请求
